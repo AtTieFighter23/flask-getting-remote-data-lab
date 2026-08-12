9da05d3894d5dd985f539db0454642d15bafcdaf
AJ <alfcabrales@gmail.com>
2026-08-11 19:43:17 -0600

    Implement GetRequester class with get_response_body and load_json methods

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """Send an HTTP GET request and return the raw response body."""
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        """Parse the raw response body into a Python data structure."""
+        return json.loads(self.get_response_body())
